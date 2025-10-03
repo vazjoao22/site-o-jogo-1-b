a93fc34d76ab44bc39a559f7613762fca121e258
vazjoao22 <vaz.joao22@escola.pr.gov.br>
2025-10-03 14:25:04 -0300

    alteraçao

---
+++ script.js
@@ -0,0 +1,51 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca. forEach(button => {
+    button.addEventListener('click'. function(){
+       const atual = document,querySelector('.ativo')  
+       const proximo = 'passo-' + this.GatAttribute('gata-proximo')
+    
+       atual.classlist.remove('ativo');
+       document.getElementById(proximoPasso)classlist.add('ativo');
+       
+    
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
